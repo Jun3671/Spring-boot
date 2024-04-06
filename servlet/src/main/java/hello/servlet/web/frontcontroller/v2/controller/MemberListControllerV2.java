3543054ea81f9ce3f36040c6b25d22be5c61d943
Jun3671 <wnsgur3027@naver.com>
2024-04-06 17:41:21 +0900

    View 분리 v2

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.frontcontroller.v2.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("utf-8");
+
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members",members); // 모델에 담을때 member가 키가 됨
+
+        return new MyView( "/WEB-INF/views/members.jsp");
+    }
+}
